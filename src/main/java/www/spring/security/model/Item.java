e5f1f51937b406de287546f6829e61ad058be1c0
yck <ck.you@wemakeprice.co.kr>
2014-02-24 10:43:58 +0900

    작업

--- src/main/java/www/spring/security/model/Item.java
+++ src/main/java/www/spring/security/model/Item.java
@@ -1,4 +1,4 @@
-package www.spring.security.dto;
+package www.spring.security.model;
 
 import java.io.Serializable;
 
@@ -8,24 +8,50 @@ import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.sql.Blob;
+import java.sql.Date;
+
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+
+@Entity
+@Table(name="ITEM")
 public class Item implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	
+	@Id
+	@Column(name = "ITEM_ID")
 	private Integer itemId;
 
 	@NotEmpty
+	@Column(name="ITEM_NAME")
 	private String itemName;
 
 	@NotNull
 	@Min(0)
+	@Column(name="PRICE")
 	private Integer price;
 
 	@NotEmpty
+	@Column(name="DESCRIPTION")
 	private String description;
 	
+	@Column(name = "PICTURE")
+	@Lob
+	private Blob picture;
+	/*
 	private MultipartFile picture;
-
+	
 	public MultipartFile getPicture() {
 		return picture;
 	}
@@ -33,11 +59,24 @@ public class Item implements Serializable {
 	public void setPicture(MultipartFile picture) {
 		this.picture = picture;
 	}
+*/
+	
+	
 
+	public Blob getPicture() {
+		return picture;
+	}
+
+	public void setPicture(Blob picture) {
+		this.picture = picture;
+	}
+
+	
 	public String getDescription() {
 		return this.description;
 	}
 
+	
 	public void setDescription(String description) {
 		this.description = description;
 	}
